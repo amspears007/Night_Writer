3d22df752883a4f21fd5ee152a80e42fc6d1db5c
Amy Spears <amspears007@gmail.com>
2023-01-14 15:26:49 -0700

    Add exists and attributes test for NightWriter class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,34 +1,31 @@
-# class NightWriter
-  # attr_accessor :read_file, :write_file
+class NightWriter
+  attr_accessor :read_file, :write_file
   
-#   def initialize
-#     @read_file = ARGV(0)
-#     @write_file = ARGV(1)
-#   end
-# end
+  def initialize
+    @read_file = ARGV[0]
+    @write_file = ARGV[1]
+  end
 
-# def call
+  def call_read_to_write
+    #a start method
+    message_file = File.open(@read_file, "r")
+    return_file = File.new(@write_file, "w")
 
-# end
+    char_count = message_file.chars.count
+
+    puts "Created #{ARGV[1]} contains #{char_count} characters"
+
+    return_file.write(message_file.read)
+    return_file.close
+  end
+end
+
 
 # #Acts a runner file outside of class
 # night_writer = NightWriter.new
 # night_writer.call
+
 #start method this new_method to translate to braille
-
-
-
-
-message_file = File.open(ARGV(0), "r")
-reuturn_file = File.new(ARGV[1], "w")
-
-char_count = message_file.chars.count
-
-puts "Created #{ARGV[1]} contains #{char_count} characters"
-
-reuturn_file.write(message_file.read)
-reuturn_file.close
-
 
 # handle = File.open(message.txt, "r")
 
