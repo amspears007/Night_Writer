034eeebfd5c54a9f24d0d9e754c8fabb03ea9103
Amy Spears <amspears007@gmail.com>
2023-01-14 11:25:52 -0700

    Add more files trying to figure out set up

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,39 @@
-handle = File.open(message.txt, "r")
+# class NightWriter
+  # attr_accessor :read_file, :write_file
+  
+#   def initialize
+#     @read_file = ARGV(0)
+#     @write_file = ARGV(1)
+#   end
+# end
 
-incoming_text = handle.read
+# def call
 
-handle.close
+# end
 
-puts incoming_text
+# #Acts a runner file outside of class
+# night_writer = NightWriter.new
+# night_writer.call
+#start method this new_method to translate to braille
+
+
+
+
+message_file = File.open(ARGV(0), "r")
+reuturn_file = File.new(ARGV[1], "w")
+
+char_count = message_file.chars.count
+
+puts "Created #{ARGV[1]} contains #{char_count} characters"
+
+reuturn_file.write(message_file.read)
+reuturn_file.close
+
+
+# handle = File.open(message.txt, "r")
+
+# incoming_text = handle.read
+
+# handle.close
+
+# puts incoming_text
